cbe4e7d338732af58d026eaabb4ba7b3e2306182
Niatsuna <niatsuna@gmail.com>
2025-12-26 20:52:27 +0100

    feat: Model API keys, implement respective db operations

--- server/src/db/schema.rs
+++ server/src/db/schema.rs
@@ -1,1 +1,15 @@
 // @generated automatically by Diesel CLI.
+
+diesel::table! {
+    api_keys (id) {
+        id -> Int4,
+        #[max_length = 255]
+        hashed_key -> Varchar,
+        #[max_length = 6]
+        key_prefix -> Varchar,
+        #[max_length = 255]
+        owner -> Varchar,
+        scopes -> Array<Text>,
+        created_at -> Timestamp,
+    }
+}
--- server/src/utils/comm/auth/models.rs
+++ server/src/utils/comm/auth/models.rs
@@ -1,1 +1,150 @@
+use chrono::NaiveDateTime;
+use diesel::{prelude::*, query_dsl::methods::FilterDsl};
+use serde::{Deserialize, Serialize};
 
+use crate::{
+    db::{
+        self, get_connection,
+        schema::{self},
+    },
+    utils::error::KohakuError,
+};
+
+// ========================================= API Keys ========================================== //
+
+/// Representation of database entry of a given ApiKey
+#[derive(Debug, Queryable, Selectable, Serialize, Deserialize)]
+#[diesel(table_name = crate::db::schema::api_keys)]
+pub struct ApiKey {
+    /// Serial Primary Key given by the database
+    pub id: i32,
+    /// Hashed presentation of the actual full key
+    pub hashed_key: String,
+    /// 10-char long prefix of the actual full key
+    pub key_prefix: String,
+    /// Identifier which service / user uses this key
+    pub owner: String,
+    /// Permission scopes given in a `category:verb` manner
+    pub scopes: Vec<String>,
+    /// Timestamp of creation (Default: Current Time UTC)
+    pub created_at: NaiveDateTime,
+}
+
+/// Form to create a new [struct@ApiKey].
+#[derive(Debug, Insertable)]
+#[diesel(table_name = crate::db::schema::api_keys)]
+pub struct NewApiKey {
+    pub hashed_key: String,
+    pub key_prefix: String,
+    pub owner: String,
+    pub scopes: Vec<String>,
+}
+
+/// Creates an entry for the API key in the database
+///
+/// # Parameters
+/// - `hashed_key` : Hashed [`String`] presentation of the actual full key
+/// - `key_prefix` : 10-char long [`String`] prefix of the actual full key
+/// - `owner` : [`String`] identifier of the service or user that uses this API key
+/// - `scopes`: Vector of [`String`]s that map the actual permissions in a `category:verb` manner
+///
+/// # Returns
+/// A [`Result`] which is either
+/// - [`Ok`] : A [struct@ApiKey] that represents the now stored API key in the database.
+/// - [`Err`] : A [enum@KohakuError] based on the failing operation.
+///
+pub async fn create_apikey(
+    hashed_key: String,
+    key_prefix: String,
+    owner: String,
+    scopes: Vec<String>,
+) -> Result<ApiKey, KohakuError> {
+    for scp in &scopes {
+        if scp.starts_with("keys") {
+            return Err(KohakuError::ValidationError("Illegal Argument: Any scope of the category `key` is not allowed for general API keys!".to_string()));
+        }
+    }
+
+    let mut conn = get_connection()?;
+
+    let new_key = NewApiKey {
+        hashed_key,
+        key_prefix,
+        owner,
+        scopes: scopes.clone(),
+    };
+
+    diesel::insert_into(schema::api_keys::table)
+        .values(&new_key)
+        .get_result(&mut conn)
+        .map_err(KohakuError::DatabaseError)
+}
+
+/// Gets an entry for an identifieable API key in the database
+///
+/// # Parameters
+/// - `id_` : Serial primary key of the database. Either this or `key_prefix` must be set
+/// - `key_prefix_` : 10-char long [`String`] prefix of the actual full key. Either this or `id` must be set
+///
+/// # Returns
+/// A [`Result`] which is either
+/// - [`Ok`] : The identified [struct@ApiKey] that matches either `id` and/or `key_prefix`
+/// - [`Err`] : A [enum@KohakuError] based on the failing operation
+pub async fn get_apikey(
+    id_: Option<i32>,
+    key_prefix_: Option<String>,
+) -> Result<ApiKey, KohakuError> {
+    use db::schema::api_keys::dsl::*;
+    if id_.is_none() && key_prefix_.is_none() {
+        return Err(KohakuError::ValidationError("Illegal Argument: At least one of the parameters - `id` and/or `key_prefix` must be set!".to_string()));
+    }
+    let mut conn = get_connection()?;
+    let mut query = api_keys.into_boxed();
+
+    if let Some(i) = id_ {
+        query = FilterDsl::filter(query, id.eq(i));
+    }
+
+    if let Some(kp) = key_prefix_ {
+        query = FilterDsl::filter(query, key_prefix.eq(kp));
+    }
+
+    query
+        .get_result(&mut conn)
+        .map_err(KohakuError::DatabaseError)
+}
+
+/// Removes an entry representing an API key from the database
+///
+/// # Parameters
+/// - `id_` : Serial primary key of the database. Either this or `key_prefix` must be set
+/// - `key_prefix_` : 10-char long [`String`] prefix of the actual full key. Either this or `id` must be set
+///
+/// # Returns
+/// A [`Result`] which is either
+/// - [`Ok`] : The API key was deleted from the database
+/// - [`Err`] : A [enum@KohakuError] based on the failing operation
+pub async fn delete_apikey(
+    id_: Option<i32>,
+    key_prefix_: Option<String>,
+) -> Result<(), KohakuError> {
+    use db::schema::api_keys::dsl::*;
+    if id_.is_none() && key_prefix_.is_none() {
+        return Err(KohakuError::ValidationError("Illegal Argument: At least one of the parameters - `id` and/or `key_prefix` must be set!".to_string()));
+    }
+    let mut conn = get_connection()?;
+    let mut query = diesel::delete(api_keys).into_boxed();
+
+    if let Some(i) = id_ {
+        query = FilterDsl::filter(query, id.eq(i));
+    }
+
+    if let Some(kp) = key_prefix_ {
+        query = FilterDsl::filter(query, key_prefix.eq(kp));
+    }
+
+    query
+        .execute(&mut conn)
+        .map_err(KohakuError::DatabaseError)?;
+    Ok(())
+}
