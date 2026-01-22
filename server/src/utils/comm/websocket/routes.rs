872305bce22a4be635ea07f59245419561f4a970
Niatsuna <niatsuna@gmail.com>
2026-01-22 02:56:28 +0100

    feat: Extend client registration with client UUID for reconnection purposes

--- server/src/utils/comm/websocket/routes.rs
+++ server/src/utils/comm/websocket/routes.rs
@@ -1,4 +1,8 @@
+use std::str::FromStr;
+
 use actix_web::{web, HttpRequest, HttpResponse};
+use actix_ws::Message;
+use serde_json::json;
 use tracing::info;
 use uuid::Uuid;
 
@@ -22,8 +26,24 @@ pub async fn ws_handler(
     }
     let verified_key = check_authorization_key(api_key.unwrap()).await?;
 
+    let client_id = req.headers().get("UUID").and_then(|h| h.to_str().ok());
+    let client_id = match client_id {
+        Some(cid) => Uuid::from_str(cid)
+            .map_err(|_| KohakuError::BadRequest("Given UUID is malformed!".to_string()))?,
+        None => Uuid::new_v4(),
+    };
+
+    let manager = get_manager()?;
+    if manager.check_connection_by_key_id(&verified_key.id)
+        || manager.check_connection_by_uuid(&client_id)
+    {
+        return Err(KohakuError::Conflict(
+            "API Key or client UUID already in use!".to_string(),
+        ));
+    }
+
     let info = WsClientInfo {
-        client_id: Uuid::new_v4(),
+        client_id,
         owner: verified_key.owner,
         key_id: verified_key.id,
     };
@@ -31,7 +51,6 @@ pub async fn ws_handler(
     let (response, session, msg_stream) =
         actix_ws::handle(&req, stream).map_err(|e| KohakuError::WebsocketError(e.to_string()))?;
 
-    let manager = get_manager()?;
     let conn = manager
         .add_connection(info.clone(), session, msg_stream)
         .await;
@@ -40,6 +59,11 @@ pub async fn ws_handler(
             "[WS - Conn] Established new connection {} for key with id {}",
             info.client_id, verified_key.id
         );
+
+        let payload = json!({ "Bearer" : client_id });
+        let content = serde_json::to_string(&payload).unwrap();
+        let _ = conn_.server_tx.send(Message::Text(content.into()));
+
         conn_.run(manager);
     } else {
         return Err(KohakuError::WebsocketError(
