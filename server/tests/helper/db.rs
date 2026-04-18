69774d02642d5ea9b17376b07b6feb775927c8ca
Niatsuna <niatsuna@gmail.com>
2026-04-18 10:27:12 +0200

    tests: Add single connection to database wrapper to ensure no state leak happens due to pooled connection manager

--- server/tests/helper/db.rs
+++ server/tests/helper/db.rs
@@ -1,26 +1,19 @@
 use std::{future::Future, pin::Pin};
 
-use diesel::{Connection as _, RunQueryDsl};
-use kohaku::{
-    db::{get_connection, migrate, schema, Connection},
-    utils::comm::auth::{
-        api_key::{generate_key, hash_key},
-        models::{ApiKey, NewApiKey},
-    },
-};
+use diesel::{Connection as _, PgConnection};
+use kohaku::db::migrate;
 
 pub async fn with_test_db<F>(f: F)
 where
-    for<'a> F: FnOnce(&'a mut Connection) -> Pin<Box<dyn Future<Output = ()> + Send + 'a>>,
+    for<'a> F: FnOnce(&'a mut PgConnection) -> Pin<Box<dyn Future<Output = ()> + Send + 'a>>,
 {
     dotenvy::dotenv().ok();
 
     let test_url = std::env::var("TEST_DATABASE_URL")
         .expect("TEST_DATABASE_URL must be set in .env for integration and e2e tests!");
-    std::env::set_var("DATABASE_URL", test_url);
 
-    let mut conn = get_connection().expect("Failed to get test DB connection");
-    migrate().expect("Failed to migrate test database");
+    let mut conn = PgConnection::establish(&test_url).expect("Failed to connect to database");
+    migrate(&mut conn).expect("Failed to migrate test database");
     conn.begin_test_transaction()
         .expect("Failed to begin test transaction");
 
